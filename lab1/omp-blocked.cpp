130327d6dcbf4fc7b38680684628eec5e9b63c06
gewing97 <gewing@ucla.edu>
2019-01-24 13:28:15 -0800

    first shot at tiling

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -6,11 +6,45 @@
 
 // Using declarations, if any...
 
-const int PROD_BLOCK_SIZE = 24;
-const int MULT_BLOCK_SIZE = 64;
+const int PROD_BLOCK_SIZE = 512;
+const int MULT_BLOCK_SIZE = 512;
 const int BLOCK_SIZE = 2;
 
-void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
+
+
+
+
+void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[kI][kJ]) {
+    #pragma omp parallel for 
+    for (int i = 0; i < kI; ++i) {
+        std::memset(c[i], 0, sizeof(float) * kJ);
+    }
+    int const vert_block_size = 20;
+    int const horz_block_size = 20;
+    #pragma omp parallel for
+    for(int vertical = 0; vertical < kI; vertical += vert_block_size){
+        for(int horizontal = 0; horizontal < kJ; horizontal += horz_block_size){
+            int vert_limit = std::min(vertical + vert_block_size, kI);
+            for(int i = 0; i < vert_limit; i++){
+                int horz_limit = std::min(horizontal + horz_block_size, kJ);
+                for(int j = 0; j < horz_limit; j++){
+                    for(int k = 0; k < vert_limit; k++){
+                        c[i][j] = a[i][k] * b[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+void GemmParallelBlocked_first_attempt(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
     #pragma omp parallel for 
     for (int i = 0; i < kI; ++i) {
