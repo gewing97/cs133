7f9c41350b306c3053e4b6a9c48be3177d71b027
gewing97 <gewing@ucla.edu>
2019-01-24 15:52:08 -0800

    typo

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -23,7 +23,7 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
             // std::printf("vert min %d", vert_limit);
             for(int i = 0; i < vert_limit; i++){
                 int horz_limit = std::min(horizontal + horz_block_size, kK);
-                for(int k = 0; k < vert_limit; k++){
+                for(int k = 0; k < horz_limit; k++){
                     for(int j = 0; j < kJ; j += 2){
                         c[i][j] = a[i][k] * b[k][j];
                         c[i][j+1] = a[i][k] * b[k][j+1];
