1eb6a215170db4bc0c6fd2196941a01156fe242e
gewing97 <gewing@ucla.edu>
2019-01-24 00:49:21 -0800

    edge error

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -19,13 +19,15 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
     #pragma omp parallel for
     for(int prod_row = 0; prod_row < kI; prod_row += PROD_BLOCK_SIZE){
         for(int mult_row = 0; mult_row < kK; mult_row += MULT_BLOCK_SIZE){
-            for(int j = 0; j < kJ; j += BLOCK_SIZE){
-                for(int i = prod_row; i < prod_row + PROD_BLOCK_SIZE; i += 2){
+            for(int j = 0; j < kJ; j += 2){
+                int end_prod_row = std::min(prod_row + PROD_BLOCK_SIZE, kI);
+                for(int i = prod_row; i < int_prod_row; i += 2){
                     float temp_a = c[i][j];
                     float temp_b = c[i][j+1];
                     float temp_c = c[i+1][j];
                     float temp_d = c[i+1][j+1];
-                    for(int k = mult_row; k < mult_row + MULT_BLOCK_SIZE; k++){
+                    int end_mult_row = std::min(mult_row + MULT_BLOCK_SIZE, kK);
+                    for(int k = mult_row; k < end_mult_row; k++){
                         temp_a += a[i][k] * b[k][j]; 
                         temp_b += a[i][k] * b[k][j+1];
                         temp_c += a[i+1][k] * b[k][j];
