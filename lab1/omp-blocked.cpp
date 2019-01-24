a51d10e08f0aa898b276eeca8dbb5aed28237d25
gewing97 <gewing@ucla.edu>
2019-01-24 00:13:03 -0800

    implementing blocks

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -6,7 +6,9 @@
 
 // Using declarations, if any...
 
-const int BLOCK_SIZE = 64;
+const int PROD_BLOCK_SIZE = 64;
+const int MULT_BLOCK_SIZE = 20;
+const int BLOCK_SIZE = 2;
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
@@ -15,4 +17,15 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
         std::memset(c[i], 0, sizeof(float) * kJ);
     }
     #pragma omp parallel for
+    for(int prod_row = 0; prod_row < kI; prod_row += PROD_BLOCK_SIZE){
+        for(int mult_row = 0; mult_row < kK; mult_row += MULT_BLOCK_SIZE){
+            for(int j = 0; j < kJ; j++){
+                for(int i = prod_row; i < prod_row + PROD_BLOCK_SIZE; i++){
+                    for(int k = mult_row; k < mult_row + MULT_BLOCK_SIZE; k++){
+                        c[i][j] = a[i][k] * b[k][j];
+                    }
+                }
+            }
+        }
+    }
 }
