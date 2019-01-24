bdf4e39248b7afffb893db901863808c94ee83f6
gewing97 <gewing@ucla.edu>
2019-01-24 15:47:36 -0800

    little unrolling

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -20,12 +20,13 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
     for(int vertical = 0; vertical < kI; vertical += vert_block_size){
         for(int horizontal = 0; horizontal < kJ; horizontal += horz_block_size){
             int vert_limit = std::min(vertical + vert_block_size, kI);
-            std::printf("vert min %d", vert_limit);
+            // std::printf("vert min %d", vert_limit);
             for(int i = 0; i < vert_limit; i++){
                 int horz_limit = std::min(horizontal + horz_block_size, kJ);
                 for(int k = 0; k < vert_limit; k++){
-                    for(int j = 0; j < horz_limit; j++){
+                    for(int j = 0; j < kJ; j += 2){
                         c[i][j] = a[i][k] * b[k][j];
+                        c[i][j+1] = a[i][k] * b[k][j+1];
                     }
                 }
             }
