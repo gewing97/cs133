a9fbf53b026603a52c57cbd58ccb90be152e3bdb
Ubuntu <ubuntu@ip-172-31-26-202.us-west-1.compute.internal>
2019-01-25 03:09:03 +0000

    unroll that k baby

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -27,11 +27,12 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
             for(i = vertical; i < vert_limit; i++){
                 horz_limit = horizontal + horz_block_size <= kK ? (horizontal + horz_block_size) : kJ;
                 //horz_limit = horizontal + horz_block_size;
-                for(k = horizontal; k < horz_limit; k++){
+                for(k = horizontal; k < horz_limit; k+=8){
                     for(j = 0; j < kJ; j++){
-                        c[i][j] += a[i][k] * b[k][j];
-                    }
-                }
+                        c[i][j] += (a[i][k] * b[k][j]) + (a[i][k+1] * b[k+1][j]) + (a[i][k+2] * b[k+2][j]) + (a[i][k+3] * b[k+3][j]) + (a[i][k+4] * b[k+4][j]) + (a[i][k+5] * b[k+5][j]) + (a[i][k+6] * b[k+6][j]) + (a[i][k+7] * b[k+7][j]);
+			
+       		    }
+		}
             }
         }
    }
