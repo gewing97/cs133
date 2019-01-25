f290e81de3fe4cdad7e674df69c42f042c89ce37
gewing97 <gewing@ucla.edu>
2019-01-24 19:13:16 -0800

    unrolled too much

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -33,11 +33,11 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
                             (a[i][k] * b[k][j]) + (a[i][k+1] * b[k+1][j]) + 
                             (a[i][k+2] * b[k+2][j]) + (a[i][k+3] * b[k+3][j]) + 
                             (a[i][k+4] * b[k+4][j]) + (a[i][k+5] * b[k+5][j]) + 
-                            (a[i][k+6] * b[k+6][j]) + (a[i][k+7] * b[k+7][j]) +
-                            (a[i][k+8] * b[k+8][j]) + (a[i][k+9] * b[k+9][j]) + 
-                            (a[i][k+10] * b[k+10][j]) + (a[i][k+11] * b[k+11][j]) + 
-                            (a[i][k+12] * b[k+12][j]) + (a[i][k+13] * b[k+13][j]) + 
-                            (a[i][k+14] * b[k+14][j]) + (a[i][k+15] * b[k+15][j]);
+                            (a[i][k+6] * b[k+6][j]) + (a[i][k+7] * b[k+7][j]);
+                            // (a[i][k+8] * b[k+8][j]) + (a[i][k+9] * b[k+9][j]) + 
+                            // (a[i][k+10] * b[k+10][j]) + (a[i][k+11] * b[k+11][j]) + 
+                            // (a[i][k+12] * b[k+12][j]) + (a[i][k+13] * b[k+13][j]) + 
+                            // (a[i][k+14] * b[k+14][j]) + (a[i][k+15] * b[k+15][j]);
                     }
                 }
             }
