52d1fb7dd483d370f3e99ed825c2c92c15e23cd7
gewing97 <gewing@ucla.edu>
2019-01-24 17:41:20 -0800

    moved initilization outside loop

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -11,21 +11,25 @@
 
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[kI][kJ]) {
-    int const vert_block_size = 512;
-    int const horz_block_size = 512;
-    #pragma omp parallel for schedule(dynamic, 16)
+    #pragma omp parallel for 
+    for (int i = 0; i < kI; ++i) {
+        std::memset(c[i], 0, sizeof(float) * kJ);
+    }
+
+    int const vert_block_size = 16;
+    int const horz_block_size = 16;
+    #pragma omp parallel for schedule(dynamic)
     for(int vertical = 0; vertical < kI; vertical += vert_block_size){
         for(int horizontal = 0; horizontal < kK; horizontal += horz_block_size){
             int vert_limit = std::min(vertical + vert_block_size, kI);
             for(int i = vertical; i < vert_limit; i++){
                 int horz_limit = std::min(horizontal + horz_block_size, kK);
                 for(int k = horizontal; k < horz_limit; k++){
-                    if(k == 0){
-                        std::memset(c[i], 0, sizeof(float) * kJ);
-                    }
-                    for(int j = 0; j < kJ; j += 2){
+                    // if(k == 0){
+                    //     std::memset(c[i], 0, sizeof(float) * kJ);
+                    // }
+                    for(int j = 0; j < kJ; j++){
                         c[i][j] += a[i][k] * b[k][j];
-                        c[i][j+1] += a[i][k] * b[k][j+1];
                     }
                 }
             }
