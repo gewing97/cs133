0ed3c627b67f8d35acb4902613b34864cdee7d16
gewing97 <gewing@ucla.edu>
2019-01-24 16:02:22 -0800

    no more debug

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 #include <iostream>
+#include <omp.h>
 
 // Using declarations, if any...
 
@@ -20,14 +21,14 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
     for(int vertical = 0; vertical < kI; vertical += vert_block_size){
         for(int horizontal = 0; horizontal < kK; horizontal += horz_block_size){
             int vert_limit = std::min(vertical + vert_block_size, kI);
-            if(omp_get_thread_num() == 0){
-                std::printf("vert lim %d \n", vert_limit);
-            }
+            // if(omp_get_thread_num() == 0){
+            //     std::printf("vert lim %d \n", vert_limit);
+            // }
             for(int i = vertical; i < vert_limit; i++){
                 int horz_limit = std::min(horizontal + horz_block_size, kK);
-                if(omp_get_thread_num() == 0){
-                    std::printf("horz lim %d \n", horz_limit);
-                }
+                // if(omp_get_thread_num() == 0){
+                //     std::printf("horz lim %d \n", horz_limit);
+                // }
                 for(int k = horizontal; k < horz_limit; k++){
                     for(int j = 0; j < kJ; j += 2){
                         c[i][j] = a[i][k] * b[k][j];
