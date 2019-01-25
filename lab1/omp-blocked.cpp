65387343c0e47928e1e703d67f3a39b26a38180e
gewing97 <gewing@ucla.edu>
2019-01-24 19:39:31 -0800

    reduced number of calculations for values otherwise constant

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -21,12 +21,12 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],  float c[
     int vert_limit, horz_limit, vertical, horizontal, i, k, j;
     #pragma omp parallel for schedule(dynamic) private(vert_limit, horz_limit, vertical, horizontal, i, k, j)
     for(vertical = 0; vertical < kI; vertical += vert_block_size){
+        //vert_limit = vertical + vert_block_size <= kI ? (vertical + vert_block_size) : kI;
+        vert_limit = vertical + vert_block_size;
         for(horizontal = 0; horizontal < kK; horizontal += horz_block_size){
-            //vert_limit = vertical + vert_block_size <= kI ? (vertical + vert_block_size) : kI;
-            vert_limit = vertical + vert_block_size;
+            //horz_limit = horizontal + horz_block_size <= kK ? (horizontal + horz_block_size) : kJ;
+            horz_limit = horizontal + horz_block_size;
             for(i = vertical; i < vert_limit; i++){
-                //horz_limit = horizontal + horz_block_size <= kK ? (horizontal + horz_block_size) : kJ;
-                horz_limit = horizontal + horz_block_size;
                 for(k = horizontal; k < horz_limit; k+=8){
                     for(j = 0; j < kJ; j++){
                         c[i][j] += 
