a9e628a6d6b6d2455ac378f8b9308450e863146b
Graham Wing <gewing@ucla.edu>
2019-02-18 14:49:04 -0800

    adding in basic blocked code from last lab

--- lab2/lab2/mpi.cpp
+++ lab2/lab2/mpi.cpp
@@ -8,5 +8,28 @@
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
-  // Your code goes here...
+    for (int i = 0; i < kI; ++i) {
+        std::memset(c[i], 0, sizeof(float) * kJ);
+    }
+
+    int vert_limit, horz_limit, vertical, horizontal, i, k, j;
+    for(vertical = 0; vertical < kI; vertical += VERT_BLOCK_SIZE){
+        //vert_limit = vertical + VERT_BLOCK_SIZE <= kI ? (vertical + VERT_BLOCK_SIZE) : kI;
+        vert_limit = vertical + VERT_BLOCK_SIZE;
+        for(horizontal = 0; horizontal < kK; horizontal += HORZ_BLOCK_SIZE){
+            //horz_limit = horizontal + HORZ_BLOCK_SIZE <= kK ? (horizontal + HORZ_BLOCK_SIZE) : kJ;
+            horz_limit = horizontal + HORZ_BLOCK_SIZE;
+            for(i = vertical; i < vert_limit; i++){
+                for(k = horizontal; k < horz_limit; k+=8){
+                    for(j = 0; j < kJ; j++){
+                        c[i][j] += 
+                            (a[i][k] * b[k][j]) + (a[i][k+1] * b[k+1][j]) + 
+                            (a[i][k+2] * b[k+2][j]) + (a[i][k+3] * b[k+3][j]) + 
+                            (a[i][k+4] * b[k+4][j]) + (a[i][k+5] * b[k+5][j]) + 
+                            (a[i][k+6] * b[k+6][j]) + (a[i][k+7] * b[k+7][j]);
+                    }
+                }
+            }
+        }
+   }
 }
