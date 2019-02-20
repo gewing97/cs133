5c7193e29f25db48976ba93e6bed648434bd576d
Graham Wing <gewing@ucla.edu>
2019-02-19 19:51:44 -0800

    was sending wrong part of a

--- lab2/lab2/mpi.cpp
+++ lab2/lab2/mpi.cpp
@@ -96,7 +96,7 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                 //send next portion of a
                 if(vert_limit + VERT_BLOCK_SIZE <= (offset + num_rows_per)){
                     printf("setting up request: %12X %d\n", a_requests, vert_limit + VERT_BLOCK_SIZE);
-                    MPI_Isend(a + ((proc -1) * vert_blocks_per) + (vert_limit - offset), VERT_BLOCK_SIZE * kK, MPI_FLOAT, proc, 0, MPI_COMM_WORLD, &a_requests[proc-1]);
+                    MPI_Isend(a + (proc * vert_blocks_per) + (vert_limit - offset), VERT_BLOCK_SIZE * kK, MPI_FLOAT, proc, 0, MPI_COMM_WORLD, &a_requests[proc-1]);
                 }    
             }
         }
